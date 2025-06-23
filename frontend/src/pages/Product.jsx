83d38c00121979b9a7784da6fbb0cabcbc2c2af0
RoshanPoulose <roshanpoulose10@gmail.com>
2025-06-23 16:05:50 +0530

    Remove leftover import of react-image-magnify

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { ShopContext } from "../context/ShopContext";
 import { assets } from "../assets/frontend_assets/assets";
-import ReactImageMagnify from "react-image-magnify";
+
 import RealatedProducts from "../components/RealatedProducts";
 
 const Product = () => {
